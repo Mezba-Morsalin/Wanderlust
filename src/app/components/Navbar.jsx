c3463254890f075010d21b24836dc9e3c8d831c9
Mezba Morsalin <mejbahmorsalin560@hotmail.com>
2026-05-17 17:53:27 +0600

    first commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><Link href='/'>Home</Link></li>
+                <li><Link href='/destinations'>Destinations</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
